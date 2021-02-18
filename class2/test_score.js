1870be8b6fc0ad9ca8cf23ccc3187c7fddb832dd
XingmingSitu <xingmingsitu@outlook.com>
2021-02-17 22:03:36 -0500

    Made a change to show

---
+++ class2/test_score.js
@@ -0,0 +1,12 @@
+let total = 0;
+// golden standard when using let
+
+let score1 = parseInt(prompt("Enter the first score"));
+total +=score1;
+
+let score2 = parseInt(prompt("Enter the second score"));
+total +=score2;
+
+let score3 = parseInt(prompt("Enter the third score"));
+total +=score3;
+
